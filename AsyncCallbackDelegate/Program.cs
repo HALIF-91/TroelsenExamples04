29b1357a244757896cc633600d8fc85b95d84203
ADIL <globalmail91@gmail.com>
2020-02-15 21:45:00 +0600

    Добавьте файлы проекта.

---
+++ AsyncCallbackDelegate/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace AsyncCallbackDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+    class Program
+    {
+        private static bool isDone = false;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** AsyncCallbackDelegate Example ********");
+            Console.WriteLine("Main invoked on thread {0}", Thread.CurrentThread.ManagedThreadId);
+
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult itfAR = b.BeginInvoke(10, 10, new AsyncCallback(AddComplete), "Main() thanks you for adding these numbers");
+
+            // Предположим что здесь выполнятеся какая-то другая работа
+            while (!isDone)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Working...");
+            }
+            Console.ReadLine();
+        }
+
+        private static void AddComplete(IAsyncResult itfAR)
+        {
+            Console.WriteLine("AddComplete() invoked on thread {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Your addition is  complete");
+
+            // Теперь получить результат
+            AsyncResult ar = (AsyncResult)itfAR;
+            BinaryOp b = (BinaryOp)ar.AsyncDelegate;
+            Console.WriteLine("10 + 10 is {0}", b.EndInvoke(itfAR));
+
+            // Получить информационный объект и привести его к string
+            string msg = (string)itfAR.AsyncState;
+            Console.WriteLine(msg);
+
+            // завршение вторичного потока
+            isDone = true;
+        }
+
+        private static int Add(int x, int y)
+        {
+            // Вывести идентификатор выполняющегося потока
+            Console.WriteLine("Add() invoked on thread {0}", Thread.CurrentThread.ManagedThreadId);
+
+            // Организовать паузу для моделирования длительной операции
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
---
+++ AsyncDelegate/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******* Async Delegate Invocation ******");
+
+            // Вывести идентификатор выполняющегося потока
+            Console.WriteLine("Main() invoked on thread {0}", Thread.CurrentThread.ManagedThreadId);
+
+            // Вызвать Add() во вторичном потоке
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult iftAR = b.BeginInvoke(10, 10, null, null);
+
+            // Выполнить другую работу в первичном потоке
+            Console.WriteLine("Doing more work in Main()!");
+
+            // Это сообщение продолжит выводиться до тех пор, пока не будет завершен метод Add()
+            while (!iftAR.IsCompleted)
+            {
+                Console.WriteLine("Doing more work in Mainnnnnnnnnnn()!");
+                Thread.Sleep(1000);
+            }
+
+            // Можно задавать максимальное время ожидания, если это время истекает
+            // метод WaitOne() возвращает false
+            /*while (!iftAR.AsyncWaitHandle.WaitOne(2000, false))
+            {
+                Console.WriteLine("Doing more work in Main()!");
+            }*/
+
+            // Теперь известно, что метод Add() завершен, получить результат по готовности
+            int answer = b.EndInvoke(iftAR);
+            Console.WriteLine("10 + 10 is {0}", answer);
+            Console.ReadLine();
+        }
+
+        private static int Add(int x, int y)
+        {
+            // Вывести идентификатор выполняющегося потока
+            Console.WriteLine("Add() invoked on thread {0}", Thread.CurrentThread.ManagedThreadId);
+
+            // Организовать паузу для моделирования длительной операции
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
