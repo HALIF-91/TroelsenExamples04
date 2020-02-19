0c59976ec5f60b473cc476bc403c40c632891f29
ADIL <globalmail91@gmail.com>
2020-02-20 00:03:33 +0600

    синхронизация

---
+++ MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadedPrinting
+{
+    class Printer
+    {
+        // Маркер блокировки
+        private object threadLock = new object();
+        public void PrintNumbers()
+        {
+            lock (threadLock)
+            {
+                // Вывести информацию о потоке
+                Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+
+                // Вывести числа
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    // Приостановить поток на случайный период времени
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    Console.Write($"{i}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void ShowNumbers()
+        {
+            Monitor.Enter(threadLock);
+            try
+            {
+                // Вывести информацию о потоке
+                Console.WriteLine("-> {0} is executing ShowNumbers()", Thread.CurrentThread.Name);
+
+                // Вывести числа
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    // Приостановить поток на случайный период времени
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    Console.Write($"{i}, ");
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+                Monitor.Exit(threadLock);
+            }
+        }
+    }
+}
---
+++ MultiThreadedPrinting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadedPrinting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Synchronizing Threads ********\n");
+            Printer p = new Printer();
+
+            // Создать 10 потоков, которые указывают
+            // на один и тот же метод того же самого объекта
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                //threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
+                threads[i] = new Thread(new ThreadStart(p.ShowNumbers));
+                threads[i].Name = String.Format("Worker thread #{0}", i);
+            }
+
+            // Теперь запустить их все
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
