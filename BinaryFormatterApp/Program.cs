3eb280eb18f43ad01a87abea6b0774ec901aa242
ADIL <globalmail91@gmail.com>
2020-02-29 15:13:48 +0600

    Сериализация

---
+++ BinaryFormatterApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinaryFormatterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserPrefs userData = new UserPrefs();
+            userData.WindowColor = "Yellow";
+            userData.FontSize = 50;
+
+            // BinaryFormatter сохраняет данные в двоичном формате
+            BinaryFormatter binFormat = new BinaryFormatter();
+
+            // Сохранить объект в локальном файле
+            using(Stream stream = new FileStream("user.dat", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(stream, userData);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BinaryFormatterApp/UserPrefs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryFormatterApp
+{
+    [Serializable]
+    public class UserPrefs
+    {
+        public string WindowColor { get; set; }
+        public int FontSize { get; set; }
+    }
+}
